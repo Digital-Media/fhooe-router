c392fc613ff805980fc21d237b3cdf933c8d382e
MH <18611980+martinharrer@users.noreply.github.com>
2020-06-17 15:04:12 +0200

    added new routes for Login and Register

--- public/index.php
+++ public/index.php
@@ -1,28 +1,42 @@
 <?php
 require "../vendor/autoload.php";
 
-use Demo\Demo;
 use Router\Router;
+use Demo\Demo;
+use Exercises\Login;
+use Exercises\Register;
 
 $route = Router::getRoute();
 switch ($route) {
     case 'GET /' :
-        $demo = new Demo("demo.html.twig");
-        $demo->show();
-        break;
-    case 'POST /' :
-        $demo = new Demo("demo.html.twig");
-        $demo->normForm();
-        break;
-    case '' :
-        $demo = new Demo("demo.html.twig");
-        $demo->show();
-        break;
-    case 'GET /about' :
-        $template = file("../templates/about.html.twig");
+        $template = file("../templates/index.html.twig");
         foreach ($template as $line)  {
             echo $line;
         }
+        break;
+    case 'GET /demo' :
+        $demo = new Demo("demo.html.twig");
+        $demo->show();
+        break;
+    case 'POST /demo' :
+        $demo = new Demo("demo.html.twig");
+        $demo->normForm();
+        break;
+    case 'GET /login' :
+        $login = new Login("login.html.twig");
+        $login->show();
+        break;
+    case 'POST /login' :
+        $login = new Login("login.html.twig");
+        $login->normForm();
+        break;
+    case 'GET /register' :
+        $register = new Register("register.html.twig");
+        $register->show();
+        break;
+    case 'POST /register' :
+        $register = new Register("register.html.twig");
+        $register->normForm();
         break;
     default:
         http_response_code(404);
