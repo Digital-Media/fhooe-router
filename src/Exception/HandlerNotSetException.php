c55ef4af92676cf10089f984f9aa0e93e5528e36
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2024-01-21 01:52:49 +0100

    Removed the second author from Exception class

--- src/Exception/HandlerNotSetException.php
+++ src/Exception/HandlerNotSetException.php
@@ -8,7 +8,6 @@ use LogicException;
  * Exception that can be thrown when a callback for a certain action is not specified.
  * @package Fhooe\Router\Exception
  * @author Wolfgang Hochleitner <wolfgang.hochleitner@fh-hagenberg.at>
- * @author Martin Harrer <martin.harrer@fh-hagenberg.at>
  * @since 0.1.0
  */
 class HandlerNotSetException extends LogicException
