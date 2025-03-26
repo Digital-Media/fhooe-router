45be7cd39f32b21cd6595f81e09973f23ce03594
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2025-03-26 09:14:37 +0100

    Refactored RouteAlreadyExistsException
    Replaced qualifier with an import. File now follows PER-CS 2.0.0.

--- src/Exception/RouteAlreadyExistsException.php
+++ src/Exception/RouteAlreadyExistsException.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Fhooe\Router\Exception;
 
+use RuntimeException;
+
 /**
  * Exception thrown when attempting to add a route that already exists.
  * @package Fhooe\Router\Exception
  * @author Wolfgang Hochleitner <wolfgang.hochleitner@fh-hagenberg.at>
  * @since 2.0.0
  */
-class RouteAlreadyExistsException extends \RuntimeException
-{
-} 
+class RouteAlreadyExistsException extends RuntimeException {}
